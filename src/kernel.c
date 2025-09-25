991c5f22bf9f6c742e796a82d8bb097b4bca16d0
rhasler1 <rhasler@luc.edu>
2025-09-25 12:45:21 -0500

    hello world

---
+++ src/kernel.c
@@ -0,0 +1,27 @@
+
+void kernel_main(void) {
+    while (1) {}
+}
+
+// For QEMU's -machine virt, the UART is an ARM PL011 at address 0x09000000
+// volatile: prevent compiler optimization & caching 
+#define UART0 ((volatile unsigned int *)0x09000000)
+
+static void uart_putc(char c) {
+    // Check if UART is ready to transmit by checking
+    // Flag Register at UART0 + 0x18
+    while (*(UART0 + 0x18/4) & (1<<5)) ;
+
+    // Writes this byte into the UART's Data Register at offset 0x00
+    // This enqueues the byte into the UART's transmit FIFO.
+    *UART0 = c;
+}
+
+void uart_puts(const char *s) {
+    while (*s) {
+        if (*s == '\n') {
+            uart_putc('\r');        // add carriage return for terminal compatibility
+        }
+        uart_putc(*s++);            // send the character
+    }
+}
